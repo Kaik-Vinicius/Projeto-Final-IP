a3784d06fd3a642a6f90688a70db1a9080f72df5
Ivan Nicholas <inicholas50@gmail.com>
2026-06-16 22:09:56 -0300

    apliquei os limites de campo para os coletaveis

--- entidades/coletaveis.py
+++ entidades/coletaveis.py
@@ -1,8 +1,9 @@
 import pygame
 import random
-import math
-from gerenciamento.constants import (LARGURA_TELA, ALTURA_TELA, COR_BOLA, 
-                       COR_ESTRELA, COR_CHUTEIRA, TAMANHO_ITEM, TEMPO_CHUTEIRA)
+from gerenciamento.constants import (LIMITE_ESQUERDO, LIMITE_DIREITO, 
+                                     LIMITE_SUPERIOR, LIMITE_INFERIOR, 
+                                     TAMANHO_ITEM, COR_ESTRELA, 
+                                     COR_CHUTEIRA, TEMPO_CHUTEIRA)
 
 class Coletavel(pygame.sprite.Sprite):
     def __init__(self, tipo, pos_jogador=None): 
@@ -10,40 +11,22 @@ class Coletavel(pygame.sprite.Sprite):
         self.tipo = tipo 
         self.image = pygame.Surface((TAMANHO_ITEM, TAMANHO_ITEM)) 
         
-        if self.tipo == 'bola':
-            self.image.fill(COR_BOLA)
-        elif self.tipo == 'estrela':
+        # Pinta a cor do quadrado dependendo do tipo 
+        if self.tipo == 'estrela':
             self.image.fill(COR_ESTRELA)
         elif self.tipo == 'chuteira':
             self.image.fill(COR_CHUTEIRA)
             
         self.rect = self.image.get_rect()
-        margem = 50
         
-        # --- LÓGICA DE SPAWN FRONTAL E PRÓXIMO ---
-        if pos_jogador and self.tipo in ['chuteira', 'estrela']:
-            
-            #  
-            distancia_min = 80  
-            distancia_max = 200 
-            distancia = random.randint(distancia_min, distancia_max)
-            
-            angulo = random.uniform(math.pi * 1.25, math.pi * 1.75) 
-            
-            novo_x = pos_jogador[0] + int(math.cos(angulo) * distancia)
-            novo_y = pos_jogador[1] + int(math.sin(angulo) * distancia)
-            
-            # Garante que não nasça fora do campo
-            self.rect.x = max(margem, min(LARGURA_TELA - margem, novo_x))
-            self.rect.y = max(margem, min(ALTURA_TELA - margem, novo_y))
-        else:
-            
-            self.rect.x = random.randint(margem, LARGURA_TELA - margem)
-            self.rect.y = random.randint(margem, ALTURA_TELA - margem)
+        # ---(LIMITES DO CAMPO) ---
+        self.rect.x = random.randint(LIMITE_ESQUERDO, LIMITE_DIREITO - TAMANHO_ITEM)
+        self.rect.y = random.randint(LIMITE_SUPERIOR, LIMITE_INFERIOR - TAMANHO_ITEM)
             
         self.tempo_nascimento = pygame.time.get_ticks()
 
     def update(self):
+        # O cronômetro de desaparecimento da chuteira
         if self.tipo == 'chuteira':
             tempo_atual = pygame.time.get_ticks()
             if tempo_atual - self.tempo_nascimento > TEMPO_CHUTEIRA:
